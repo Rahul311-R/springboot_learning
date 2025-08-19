049025aa7257b0fe371bfb09e6d30c92e06451ea
Rahul311-R <rahul541r@gmail.com>
2025-08-20 05:28:30 +0530

    19/08/2025

---
+++ src/main/java/com/example/springboot/source.java
@@ -0,0 +1,35 @@
+package com.example.springboot;
+
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class source {
+    @RequestMapping("home")
+    public String m(){
+        return "i am rahul";
+    }
+
+    @RequestMapping("contact")
+    public String m1(){ return "rahul@gmail.com"; }
+
+    @RequestMapping("dept")
+    public String m2(){return "Artificial intelligence";}
+
+    @RequestMapping("/home/{name}")
+    public String m3(@PathVariable String name){
+        return "hello "+name+" !";
+    }
+
+    @RequestMapping("/greet")
+    public String greet(@RequestParam(defaultValue = "rahul") String name) {
+        return "Greetings, " + name + "!";
+    }
+
+    @GetMapping("/user")
+    public User getuser(){
+        return new User("rahul",21);
+    }
+
+}
