796d750757cf9de5111ec64ab4b6185b8f1ecbbb
Rahul R <rahul541r@gmail.com>
2025-08-21 16:18:29 +0530

    21/08/2025

--- src/main/java/com/example/springboot/source.java
+++ src/main/java/com/example/springboot/source.java
@@ -1,35 +1,39 @@
 package com.example.springboot;
 
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 
 public class source {
-    @RequestMapping("home")
-    public String m(){
-        return "i am rahul";
-    }
-
-    @RequestMapping("contact")
-    public String m1(){ return "rahul@gmail.com"; }
-
-    @RequestMapping("dept")
-    public String m2(){return "Artificial intelligence";}
-
-    @RequestMapping("/home/{name}")
-    public String m3(@PathVariable String name){
-        return "hello "+name+" !";
-    }
-
-    @RequestMapping("/greet")
+//    @RequestMapping("home")
+//    public String m(){
+//        return "i am rahul";
+//    }
+//
+//    @RequestMapping("contact")
+//    public String m1(){ return "rahul@gmail.com"; }
+//
+//    @RequestMapping("dept")
+//    public String m2(){return "Artificial intelligence";}
+//
+//    @RequestMapping("/home/{name}")
+//    public String m3(@PathVariable String name){
+//        return "hello "+name+" !";
+//    }
+//
+    @GetMapping("/greet")
     public String greet(@RequestParam(defaultValue = "rahul") String name) {
         return "Greetings, " + name + "!";
     }
-
-    @GetMapping("/user")
-    public User getuser(){
-        return new User("rahul",21);
+//
+//    @GetMapping("/user")
+//    public User getuser(){
+//        return new User("rahul",21);
+//    }
+    @PostMapping("/user")
+    public User create(@RequestBody User user){
+        return user;
     }
+
 
 }
